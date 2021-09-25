6b22069c4ad1bc8e6702c3e9eb87f7c5d11b6610
Rutuja Jakwad <rutujajakwad585@gmail.com>
2021-09-25 20:40:59 +0530

    Added: Content of Message

---
+++ src/linecomparison/LinecomparisonUc1.java
@@ -0,0 +1,17 @@
+package linecomparison;
+
+public class LinecomparisonUc1 {
+	double distance(int x1 , int y1 , int x2 , int y2)
+	{
+		double distance;
+		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return distance;
+	}
+	
+	public static void main(String[] args) {
+	LinecomparisonUc1 obj = new LinecomparisonUc1();
+	double ans = obj.distance(1,2,3,4);
+	System.out.println("Distance between points is : "+ans);
+	}
+
+}
